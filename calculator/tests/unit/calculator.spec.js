be512c2fe2e5c0bb11107e857fdec5d31b8fbc23
user <user@users-MacBook-Pro-5.local>
2020-10-14 16:06:13 +0100

    First commit, 5 unit tests passing

---
+++ calculator/tests/unit/calculator.spec.js
@@ -0,0 +1,43 @@
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import App from '@/App.vue'
+
+describe('App.vue', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(App);
+  });
+
+  it('enterNum changes running total', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 4
+    wrapper.vm.add('5');
+    expect(wrapper.vm.runningTotal).to.equal(9)
+  })
+
+  it('should add 1 to 4 and get 5', () => {
+    wrapper.vm.previousTotal = 4
+    wrapper.vm.add('1');
+    expect(wrapper.vm.runningTotal).to.equal(5)
+  })
+
+  it('should subtract 4 from 7 and get 3', () => {
+    wrapper.vm.previousTotal = 7
+    wrapper.vm.subtract('4');
+    expect(wrapper.vm.runningTotal).to.equal(3)
+  })
+
+  it('should multiply 3 by 5 and get 15', () => {
+    wrapper.vm.previousTotal = 3
+    wrapper.vm.multiply('5');
+    expect(wrapper.vm.runningTotal).to.equal(15)
+  })
+
+  it('should deivide 21 by 7 and get 3', () => {
+    wrapper.vm.previousTotal = 21
+    wrapper.vm.divide('7');
+    expect(wrapper.vm.runningTotal).to.equal(3)
+  })
+
+})
