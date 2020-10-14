e6a2c8818ed4e689b581a1756d02b1df13c9a911
David Strain <strd79@hotmail.co.uk>
2020-10-14 18:51:35 +0100

    Commit with 5 e2e tests passing

--- calculator/tests/e2e/specs/test.js
+++ calculator/tests/e2e/specs/test.js
@@ -9,4 +9,50 @@ describe('calculator', () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('should have all working number buttons', () => {
+    cy.get('#number1').click();
+    cy.get('#number2').click();
+    cy.get('#number3').click();
+    cy.get('#number4').click();
+    cy.get('#number5').click();
+    cy.get('#number6').click();
+    cy.get('#number7').click();
+    cy.get('#number8').click();
+    cy.get('#number9').click();
+    cy.get('#number0').click();
+    cy.get('.display').should('contain', '1234567890');
+  })
+
+  it('should have arithmetical operators update the display with the result of the operation', () => {
+    cy.get('#number8').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number4').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '2');
+  })
+
+  it('should allow multiple operations to be chanined together', () => {
+    cy.get('#number1').click();
+    cy.get('#operator_add').click();
+    cy.get('#number3').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number5').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number1').click();
+    cy.get('#number0').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number9').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '-7');
+  })
+
+  it('should display a decimal number', () => {
+    cy.get('#number6').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number7').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '0.8571')
+  })
+
 })
